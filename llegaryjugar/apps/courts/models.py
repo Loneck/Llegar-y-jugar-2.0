aa899d57603645c2bdf9d92d90b1ba2377e359f5
Loneck <leo.torrejon@gmail.com>
2017-04-18 11:33:12 -0300

    models upgrade

--- llegaryjugar/apps/courts/models.py
+++ llegaryjugar/apps/courts/models.py
@@ -5,7 +5,7 @@ from llegaryjugar.apps.clubs.models import Clubs
 from llegaryjugar.apps.courts_status.models import Court_status
 
 class Courts(models.Model):
-	club = models.ForeignKey(Clubs, related_name='court', verbose_name=_('club'))
+	club = models.ForeignKey(Clubs, related_name='club', verbose_name=_('club'))
 	status = models.ForeignKey(Court_status, related_name='status', verbose_name=_('status'))
 	name = models.CharField(_('name'), max_length=50)
 	number = models.CharField(_('number'), max_length=50)
--- llegaryjugar/apps/reservations/models.py
+++ llegaryjugar/apps/reservations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext as _
+from django.utils import timezone
+from django.db import models
+from llegaryjugar.apps.schedules.models import Schedules 
+from llegaryjugar.apps.courts.models import Courts 
+from llegaryjugar.apps.payment_methods.models import Payment_methods 
+from llegaryjugar.apps.services.models import Services 
+from llegaryjugar.apps.reservations_status.models import Reservations_status
+
+class Courts(models.Model):
+	schedule = models.ForeignKey(Schedules, related_name='schedules', verbose_name=_('schedules'))
+	court = models.ForeignKey(Courts, related_name='courts', verbose_name=_('courts'))
+	client = models.ForeignKey('auth.User')
+	payment = models.ForeignKey(Payment_methods, related_name='payment', verbose_name=_('payment'))
+	service = models.ForeignKey(Services, related_name='service', verbose_name=_('service'))
+	status = models.ForeignKey(Reservations_status, related_name='reservations_status', verbose_name=_('reservations_status'))
+	created_date = models.DateTimeField(default=timezone.now)
