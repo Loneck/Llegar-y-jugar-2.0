3816ad32c53252b0a1252bbf3e82d27e91488fa6
Cristian Sepulveda <cristian@magnet.cl>
2017-09-02 20:47:58 -0300

    add signal with multiples m2m on ScheduleCreate

--- llegaryjugar/apps/courts/models.py
+++ llegaryjugar/apps/courts/models.py
@@ -7,10 +7,24 @@ from llegaryjugar.apps.courts_status.models import CourtStat
 
 
 class Court(BaseModel):
-    club = models.ForeignKey(Club, related_name='club', verbose_name=_('club'))
-    status = models.ForeignKey(CourtStat, related_name='court_status', verbose_name=_('status'))
-    name = models.CharField(_('name'), max_length=50)
-    number = models.CharField(_('number'), max_length=50)
+    club = models.ForeignKey(
+        Club,
+        related_name='club',
+        verbose_name=_('club'),
+    )
+    status = models.ForeignKey(
+        CourtStat,
+        related_name='court_status',
+        verbose_name=_('status'),
+    )
+    name = models.CharField(
+        _('name'),
+        max_length=50,
+    )
+    number = models.CharField(
+        _('number'),
+        max_length=50,
+    )
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
--- llegaryjugar/apps/schedules/models.py
+++ llegaryjugar/apps/schedules/models.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta, date
+
 from django.db import models
-# from django.db.models import signals
-# from django.dispatch import receiver
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 from django.utils.translation import ugettext as _
+
 from llegaryjugar.apps.base.models import BaseModel
 from llegaryjugar.apps.courts.models import Court
 from llegaryjugar.apps.dates.models import Day, Month, Hour
-from datetime import datetime, timedelta, date
-# import django.dispatch
-
-# m2m_post_save = django.dispatch.Signal(providing_args=["instance"])
 
 
 class Schedule(BaseModel):
@@ -27,11 +26,18 @@ class Schedule(BaseModel):
         _('Date'),
         default=date.today
     )
-    start_time = models.TimeField(_('start time'))
-    end_time = models.TimeField(_('end time'))
+    start_time = models.TimeField(
+        _('start time'),
+    )
+    end_time = models.TimeField(
+        _('end time'),
+    )
 
     def __str__(self):
-        return '%s %s' % (self.start_time, self.end_time)
+        return '{} {}'.format(
+            self.start_time,
+            self.end_time,
+        )
 
 
 class SchedulesCreate(BaseModel):
@@ -46,15 +52,31 @@ class SchedulesCreate(BaseModel):
         verbose_name=_('court'),
         null=True,
     )
-    price = models.DecimalField(_('price'), decimal_places=2, max_digits=30)
+    price = models.DecimalField(
+        _('price'),
+        decimal_places=2,
+        max_digits=30,
+    )
     day = models.ManyToManyField(
-        Day, related_name='day_create', verbose_name=_('day'))
+        Day,
+        related_name='day_create',
+        verbose_name=_('day'),
+    )
     month = models.ManyToManyField(
-        Month, related_name='month_create', verbose_name=_('month'))
+        Month,
+        related_name='month_create',
+        verbose_name=_('month'),
+    )
     start_time = models.ForeignKey(
-        Hour, related_name='start_time_hour', default=True)
+        Hour,
+        related_name='start_time_hour',
+        null=True
+    )
     end_time = models.ForeignKey(
-        Hour, related_name='end_time_hour', default=True)
+        Hour,
+        related_name='end_time_hour',
+        null=True
+    )
 
     def day_in_month(self):
         days = self.day.values_list('day', flat=True)
@@ -63,23 +85,22 @@ class SchedulesCreate(BaseModel):
         return days, months
 
     def create_schedules(self):
-        format = "%H:%M"
-        print "estoy aquí"
+        hour_format = "%H:%M"
         start_time = str(self.start_time)
         end_time = str(self.end_time)
 
         if end_time == '24:00':
             end_time = '23:59'
-        start_time = datetime.strptime(start_time, format)
-        end_time = datetime.strptime(end_time, format)
+        start_time = datetime.strptime(start_time, hour_format)
+        end_time = datetime.strptime(end_time, hour_format)
 
         schedule_list = []
         while start_time <= end_time:
-            start_time.strftime(format)
-            schedule_list.append(start_time.strftime(format))
+            start_time.strftime(hour_format)
+            schedule_list.append(start_time.strftime(hour_format))
             start_time += timedelta(minutes=60)
 
-        midnight = end_time.strftime(format)
+        midnight = end_time.strftime(hour_format)
         if midnight == '23:59':
             schedule_list.append('00:00')
 
@@ -93,32 +114,25 @@ class SchedulesCreate(BaseModel):
                 price=price, start_time=start, end_time=end, court=court)
 
     def save(self, *args, **kwargs):
-        self.create_schedules()
-        super(SchedulesCreate, self).save(*args, **kwargs)
+        save = super(SchedulesCreate, self).save(*args, **kwargs)
+        return save
 
-    def __str__(self):
-        return '%s %s' % (self.author, self.court)
-    # def save_related(self, request, form, formsets, change):
-    #     super(SchedulesCreate, self).save_related(request, form, formsets, change)
-    #     m2m_post_save.send(sender=self.__class__, instance=form.instance)
-
-
-# @receiver(m2m_changed, sender=SchedulesCreate, dispatch_uid='day_schedule_create')
-# def day_schedule_create(sender, instance, action, *args, **kwargs):
-#     print "afuera del if"
-#     if action == 'post_add':
-#         print "estoy acá"
-#         instance.create_schedules()
-#         print "estoy aqulla"
-# def create_schedule(sender, **kwargs):
-#     if kwargs['created']:
-#         obj = kwargs['instance']
-#         print ("alguna wea", obj.price)
-#         print ("Dias", obj.day.all())
-#         days_list = obj.day.all()
-#         for days in days_list:
-#             print ("Días", days)
+    def __unicode__(self):
+        return '{} {}'.format(
+            self.author.get_full_name(),
+            self.court.name,
+        )
 
 
-# signals.post_save.connect(create_schedule, sender=SchedulesCreate)
-# m2m_post_save.connect(day_schedule_create, sender=SchedulesCreate)
+@receiver(
+    m2m_changed,
+    sender=SchedulesCreate.day.through,
+    dispatch_uid='day_schedule_create')
+@receiver(
+    m2m_changed,
+    sender=SchedulesCreate.month.through,
+    dispatch_uid='day_schedule_create')
+def day_schedule_create(sender, instance, action, *args, **kwargs):
+    if (action == 'post_add'and
+            instance.day.exists() and instance.month.exists()):
+        instance.create_schedules()
